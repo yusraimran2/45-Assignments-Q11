71e248115da1da61426b733b0be4f5f5009f1ea0
yusraimran2 <yusraimran12@hotmail.com>
2024-11-12 20:54:48 +0500

    Add files via upload

---
+++ Q11.ts
@@ -0,0 +1,7 @@
+//11: Names: Store the names of a few of your friends in a array called names.Print each person's name by accessing each elements in the list,one at a time.
+ let names: string[] = ["Faryal","Novish","Nazia","Umaima"]
+ console.log(names[0])
+ console.log(names[1])
+ console.log(names[2])
+ console.log(names[3])
+
